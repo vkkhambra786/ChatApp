2147e43c73368fe6b95d18fa15402a64e85f134e
vkkhambra786 <vkkhambra786@gmail.com>
2024-03-25 13:17:27 +0530

    path file __ diename adede for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import connectToMongoBb from "./db/connectMNDB.js";
@@ -9,6 +10,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import { app, server } from "./socket/socket.js";
 //remove app from   here and use in Socket.js file
+const __dirname = path.resolve();
 //const app = express();
 const PORT = process.env.PORT || 5002;
 
@@ -18,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoBb();
